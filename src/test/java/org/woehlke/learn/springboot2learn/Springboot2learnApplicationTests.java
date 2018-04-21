517288af38783bb6f4677b87f9893611be724682
phasenraum2010 <thomas@woehlke.org>
2018-04-21 12:12:54 +0200

    inititial setup

--- src/test/java/org/woehlke/learn/springboot2learn/Springboot2learnApplicationTests.java
+++ src/test/java/org/woehlke/learn/springboot2learn/Springboot2learnApplicationTests.java
@@ -1,4 +1,4 @@
-package org.woehlke.learn.demo;
+package org.woehlke.learn.springboot2learn;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class Springboot2learnApplicationTests {
 
 	@Test
 	public void contextLoads() {
